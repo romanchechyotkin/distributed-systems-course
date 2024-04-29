b8d553722f78b03a767a7361e161904dc20c59d2
raman.chachotkin <raman.chachotkin@indev.by>
2024-04-29 11:43:25 +0300

    tls auth via acl

--- 0_log_system/internal/config/files.go
+++ 0_log_system/internal/config/files.go
@@ -9,6 +9,7 @@ var (
 	CAFile         = configFile("ca.pem")
 	ServerCertFile = configFile("server.pem")
 	ServerKeyFile  = configFile("server-key.pem")
+
 	ClientCertFile = configFile("client.pem")
 	ClientKeyFile  = configFile("client-key.pem")
 
